88821451f1ea2581def7badee31fe3421936967c
Ronel028 <floridaronel15@gmail.com>
2023-02-03 18:03:59 +0800

    make navigation responsive and working with hero section

--- src/component/navigation/navigation.jsx
+++ src/component/navigation/navigation.jsx
@@ -1,53 +1,102 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./navigation.scss";
 
 const Navigation = () => {
-  return (
-    <nav className="nav">
-      <div className="wrapper nav-container">
-        <Link to="/" className="logo">
-          IdeaHive
-        </Link>
+  const [displayMenu, setDisplayMenu] = useState(false);
 
-        {/* link for desktop */}
-        <ul className="nav-links-desktop">
+  const handleChange = () => {
+    setDisplayMenu((currentState) => !currentState);
+  };
+
+  return (
+    <div>
+      <nav className="nav">
+        <div className="wrapper nav-container">
+          <Link to="/" className="logo">
+            IdeaHive
+          </Link>
+
+          {/* link for desktop */}
+          <ul className="nav-links-desktop">
+            <li>
+              <Link to="/story" className="link-desktop">
+                Story
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className="link-desktop">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="contact" className="link-desktop">
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link to="/signin" className="link-desktop">
+                Sign in
+              </Link>
+            </li>
+            <li>
+              <Link to="/signup" className="link-desktop link-btn">
+                Sign up
+              </Link>
+            </li>
+          </ul>
+          {/* link for desktop */}
+
+          {/* menu btn */}
+          <label htmlFor="menu" className="menu-btn">
+            <input
+              type="checkbox"
+              name="checkbox"
+              id="menu"
+              onChange={handleChange}
+            />
+            <div className="line line1"></div>
+            <div className="line line2"></div>
+            <div className="line line3"></div>
+          </label>
+        </div>
+      </nav>
+      {/* menu mobile */}
+      <div
+        className="nav-links-mobile"
+        style={{ top: displayMenu ? "71px" : "" }}
+      >
+        <ul className="links">
           <li>
-            <Link to="/story" className="link-desktop">
+            <Link to="/story" className="link-mobile">
               Story
             </Link>
           </li>
           <li>
-            <Link to="/about" className="link-desktop">
+            <Link to="/about" className="link-mobile">
               About
             </Link>
           </li>
           <li>
-            <Link to="contact" className="link-desktop">
+            <Link to="contact" className="link-mobile">
               Contact
             </Link>
           </li>
+        </ul>
+        <ul className="auth">
           <li>
-            <Link to="/signin" className="link-desktop">
+            <Link to="/signin" className="link-mobile">
               Sign in
             </Link>
           </li>
           <li>
-            <Link to="/signup" className="link-desktop link-btn">
+            <Link to="/signup" className="link-mobile" id="btn">
               Sign up
             </Link>
           </li>
         </ul>
-        {/* link for desktop */}
-
-        {/* menu btn */}
-        <label htmlFor="menu" className="menu-btn">
-          <input type="checkbox" name="checkbox" id="menu" />
-          <div className="line line1"></div>
-          <div className="line line2"></div>
-          <div className="line line3"></div>
-        </label>
       </div>
-    </nav>
+    </div>
   );
 };
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,22 @@
+import { Link } from "react-router-dom";
 import Navigation from "../../component/navigation/navigation";
 import "./home.scss";
 const Home = () => {
   return (
     <div className="home">
-      <header>
+      <header className="header">
         <Navigation />
+        <main className="hero">
+          <div className="wrapper">
+            <h1>Discover insights and inspiration</h1>
+            <p>Sharing stories that inspire change</p>
+            <div className="cta-container">
+              <Link to="/story" className="hero-cta" id="btn">
+                Start Reading
+              </Link>
+            </div>
+          </div>
+        </main>
       </header>
     </div>
   );
