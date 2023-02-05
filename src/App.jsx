d9ff852d89dff33baa7bccb41b29fc99cd064a45
Ronel028 <floridaronel15@gmail.com>
2023-02-05 11:42:21 +0800

    creating register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
 import "./App.scss";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Register />} />
     </Routes>
   );
 }
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import "./register.scss";
+const Register = () => {
+  return (
+    <main className="register-container">
+      <div className="wrapper register">
+        <h1>Create an account</h1>
+        <p>
+          Already have an account?{" "}
+          <Link to="/signin" className="login-link">
+            Login
+          </Link>
+        </p>
+        <form className="register-form">
+          <div className="input-container fname">
+            <label htmlFor="fname">First Name</label>
+            <input type="text" name="fname" id="fname" />
+          </div>
+          <div className="input-container lname">
+            <label htmlFor="lname">Last Name</label>
+            <input type="text" name="lname" id="lname" />
+          </div>
+          <div className="input-container email">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" />
+          </div>
+          <div className="input-container password">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" />
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+};
+
+export default Register;
