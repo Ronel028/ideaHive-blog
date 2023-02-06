5feaee319ffb6e1d995b6343163763df5597018f
Ronel028 <floridaronel15@gmail.com>
2023-02-06 10:06:17 +0800

    creating story or blog page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
 import Signin from "./pages/signin/Signin";
+import Story from "./pages/blog/Story";
 import "./App.scss";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/signup" element={<Register />} />
       <Route path="/signin" element={<Signin />} />
+      <Route path="/story" element={<Story />} />
     </Routes>
   );
 }
---
+++ src/pages/blog/Story.jsx
@@ -0,0 +1,70 @@
+import Navigation from "../../component/navigation/navigation";
+import FeaturedBlog from "../../component/featured-blog/Featured-blog";
+import Blog from "../../component/blog/Blog";
+import featureBlogLogo from "../../assets/featured-blog.svg";
+import sampleProfile1 from "../../assets/sample-profile-1.jpeg";
+import "./story.scss";
+const Story = () => {
+  return (
+    <>
+      <header className="story-header">
+        <Navigation />
+        <main className="wrapper story-main">
+          <h1 className="heading">Stories & Ideas</h1>
+          <p className="subheading">
+            Stay ahead of the curve with the latest technology and news, and get
+            inspired by real-life stories of people
+          </p>
+        </main>
+      </header>
+      <div className="wrapper story-list">
+        <aside className="featured-blog">
+          <div className="sticky">
+            <h3 className="title">
+              <img src={featureBlogLogo} alt="featureBlog logo" />
+              Featured blog posts
+            </h3>
+            <div className="list">
+              <FeaturedBlog
+                profilePic={sampleProfile1}
+                bloggerName="Roland Bell"
+                blogTitle="UX lessons from a poet who invented social media in the 18th century"
+              />
+              <FeaturedBlog
+                profilePic={sampleProfile1}
+                bloggerName="Roland Bell"
+                blogTitle="UX lessons from a poet who invented social media in the 18th century"
+              />
+              <FeaturedBlog
+                profilePic={sampleProfile1}
+                bloggerName="Roland Bell"
+                blogTitle="UX lessons from a poet who invented social media in the 18th century"
+              />
+              <FeaturedBlog
+                profilePic={sampleProfile1}
+                bloggerName="Roland Bell"
+                blogTitle="UX lessons from a poet who invented social media in the 18th century"
+              />
+            </div>
+          </div>
+        </aside>
+        <main className="blog">
+          <Blog />
+          <Blog />
+          <Blog />
+          <Blog />
+          <Blog />
+          <Blog />
+          <Blog />
+          <Blog />
+          <Blog />
+          <Blog />
+          <Blog />
+          <Blog />
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default Story;
