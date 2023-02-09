58d95fd3bb153567d4c7bb9146c65785a867d044
Ronel028 <floridaronel15@gmail.com>
2023-02-09 22:55:07 +0800

    connect register page to back-end and perform some validation and displaying error in the ui

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,54 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import axios from "axios";
 import "./register.scss";
 const Register = () => {
+  const [inputVal, setInputVal] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState([]);
+
+  const getInputVal = (e) => {
+    const { name, value } = e.target;
+    setInputVal({
+      ...inputVal,
+      [name]: value,
+    });
+  };
+
+  const registerUser = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    Object.entries(inputVal).forEach(([name, value]) => {
+      formData.append(name, value);
+    });
+    const regUser = await axios.post("/user/register", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    setError(regUser.data.error);
+  };
+
+  // render error message
+  const errorMsg = (errorList) => {
+    if (errorList.length > 0) {
+      return (
+        <ul id="error-message">
+          {errorList.map((err) => (
+            <li key={err}>{err}</li>
+          ))}
+        </ul>
+      );
+    } else {
+      return "";
+    }
+  };
+
   return (
     <motion.main
       className="register-container"
@@ -17,22 +64,35 @@ const Register = () => {
             Login
           </Link>
         </p>
-        <form className="register-form">
+        {/* error message */}
+        {errorMsg(error)}
+
+        <form className="register-form" onSubmit={registerUser}>
           <div className="input-container fname">
             <label htmlFor="fname">First Name</label>
-            <input type="text" name="fname" id="fname" />
+            <input type="text" name="fname" id="fname" onChange={getInputVal} />
           </div>
           <div className="input-container lname">
             <label htmlFor="lname">Last Name</label>
-            <input type="text" name="lname" id="lname" />
+            <input type="text" name="lname" id="lname" onChange={getInputVal} />
           </div>
           <div className="input-container email">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              onChange={getInputVal}
+            />
           </div>
           <div className="input-container password">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              onChange={getInputVal}
+            />
           </div>
           <div className="register-btn">
             <button type="submit" id="btn">
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/user/register": "http://localhost:3000",
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
