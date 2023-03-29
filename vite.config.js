d0eca77c15639bf338b9323185ce223a1c22fb56
Ronel028 <floridaronel15@gmail.com>
2023-03-29 22:28:30 +0800

    configure new proxy server test 21

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/user/user": "https://idea-h-ive-blog.vercel.app",
-      "/blog": {
+      "/blog/blog": {
         target: "https://idea-h-ive-blog.vercel.app",
         pathRewrite: { "^/blog/blog": "/blog" },
         changeOrigin: true,
