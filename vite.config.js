ff7a3b3261993d12c260413fe2cc46710a6daef8
Ronel028 <floridaronel15@gmail.com>
2023-03-29 21:50:21 +0800

    configure new proxy server 6

--- vite.config.js
+++ vite.config.js
@@ -16,8 +16,8 @@ export default defineConfig({
   // },
   server: {
     proxy: {
-      "/user": "https://api-ideahive.onrender.com",
-      "/blog": "https://api-ideahive.onrender.com",
+      "/api": "https://api-ideahive.onrender.com",
+      // "/blog": "https://api-ideahive.onrender.com",
     },
   },
   // server: {
