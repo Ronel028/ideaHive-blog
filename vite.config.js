f3b37ed132d6f43176fefafd9644c7cd840efb2d
Ronel028 <floridaronel15@gmail.com>
2023-03-30 20:48:05 +0800

    back all code to default development

--- vite.config.js
+++ vite.config.js
@@ -3,40 +3,18 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   middleware: [
-  //     createProxyMiddleware("/user/*", {
-  //       target: "https://api-ideahive.onrender.com",
-  //     }),
-  //     createProxyMiddleware("/blog/*", {
-  //       target: "https://api-ideahive.onrender.com",
-  //     }),
-  //   ],
-  // },
-  server: {
-    proxy: {
-      "/user": "https://idea-h-ive-blog.vercel.app",
-      "/blog": "https://idea-h-ive-blog.vercel.app",
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: "/src/main.jsx",
     },
   },
-  // server: {
-  //   proxy: {
-  //     "/user/register": "https://api-ideahive.onrender.com/",
-  //     "/user/signin": "https://api-ideahive.onrender.com/",
-  //     "/user/signout": "https://api-ideahive.onrender.com/",
-  //     "/user/verified": "https://api-ideahive.onrender.com",
-  //     "/user/info": "https://api-ideahive.onrender.com",
-  //     "/user/update": "https://api-ideahive.onrender.com/",
-  //     "/user/update-password": "https://api-ideahive.onrender.com/",
-  //     "/blog/add-blog": "https://api-ideahive.onrender.com/",
-  //     "/blog/get-blog": "https://api-ideahive.onrender.com",
-  //     "/blog/user-blog": "https://api-ideahive.onrender.com/",
-  //     "/blog/blog-content": "https://api-ideahive.onrender.com/",
-  //     "/blog/delete-blog": "https://api-ideahive.onrender.com/",
-  //     "/blog/update-blog": "https://api-ideahive.onrender.com/",
-  //   },
-  //},
-
+  server: {
+    proxy: {
+      "/user": "http://localhost:3000",
+      "/blog": "http://localhost:3000",
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
