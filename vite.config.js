2f7dc4a0b64154707a7224e269564f37220b8f86
Ronel028 <floridaronel15@gmail.com>
2023-03-31 10:35:29 +0800

    ready to deploy in production

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: "/src/main.jsx",
-    },
-  },
+  // build: {
+  //   manifest: true,
+  //   rollupOptions: {
+  //     input: "/src/main.jsx",
+  //   },
+  // },
   server: {
     proxy: {
       "/user": "http://localhost:3000",
