17e61d6bee4ab79eb4db492e19b113391d7571c0
Ronel028 <floridaronel15@gmail.com>
2023-04-01 22:23:11 +0800

    add proxy server

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // build: {
-  //   manifest: true,
-  //   rollupOptions: {
-  //     input: "/src/main.jsx",
-  //   },
-  // },
   server: {
     proxy: {
-      "/user": "http://localhost:3000",
-      "/blog": "http://localhost:3000",
+      "/user": "https://api-ideahive.onrender.com",
+      "/blog": "https://api-ideahive.onrender.com",
     },
   },
   css: {
