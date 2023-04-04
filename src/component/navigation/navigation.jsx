341b0c0b05379fd079a2e5d1dc08f6c57b022455
Ronel028 <floridaronel15@gmail.com>
2023-04-04 10:02:51 +0800

    add url ito perform logout function

--- src/component/navigation/navigation.jsx
+++ src/component/navigation/navigation.jsx
@@ -146,7 +146,10 @@ const Navigation = () => {
   // function for signout user
   const signoutUser = async () => {
     try {
-      const signout = await axios.get("/user/signout");
+      const signout = await axios.get(
+        "https://idea-h-ive-blog.vercel.app/user/signout",
+        { withCredentials: true }
+      );
       if (signout.data.isLogout) {
         setUser({
           ...user,
