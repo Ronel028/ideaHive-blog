12dc3e3ee2d153bf3d3909a05b0dad4cc24f2c2c
Ronel028 <floridaronel15@gmail.com>
2023-04-06 20:11:22 +0800

    add configuration in vite.config.js in production

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,24 @@
 import { splitVendorChunkPlugin } from "vite";
 import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react(), splitVendorChunkPlugin()],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    brotliSize: false,
+    sourcemap: true,
+  },
+  server: {
+    middlewareMode: true,
+    fs: {
+      strict: true,
+    },
+    fallback: "index.html",
+  },
   css: {
     preprocessorOptions: {
       scss: {
@@ -11,5 +26,4 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react(), splitVendorChunkPlugin()],
 });
