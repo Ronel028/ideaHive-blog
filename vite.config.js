e4163f3d5e0711dbd170808541c253d68e205676
Ronel Florida <89984431+Ronel028@users.noreply.github.com>
2023-04-07 10:21:48 +0800

    update vite-config-js

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,11 @@
-import { splitVendorChunkPlugin } from "vite";
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   base: "/ideaHive-blog/",
-  plugins: [react(), splitVendorChunkPlugin()],
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    brotliSize: false,
-    sourcemap: true,
-  },
-  server: {
-    middlewareMode: true,
-    fs: {
-      strict: true,
-    },
-  },
   css: {
     preprocessorOptions: {
       scss: {
