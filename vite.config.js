f3384e98433dff1cbf8d3c2b1f1fb997e434e0c0
Ronel Florida <89984431+Ronel028@users.noreply.github.com>
2023-04-07 10:13:21 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/ideaHive-blog/",
   plugins: [react(), splitVendorChunkPlugin()],
   build: {
     outDir: "dist",
