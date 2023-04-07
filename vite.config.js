f77f67300da332dbf323e7064c094b9642140d91
Ronel028 <floridaronel15@gmail.com>
2023-04-07 09:53:51 +0800

    add base set to / in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,20 +5,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react(), splitVendorChunkPlugin()],
   build: {
     outDir: "dist",
     emptyOutDir: true,
     brotliSize: false,
     sourcemap: true,
-    rollupOptions: {
-      output: {
-        format: "es",
-      },
-    },
   },
   server: {
-    middlewareMode: "html",
+    middlewareMode: true,
     fs: {
       strict: true,
     },
